65b3968b0afb30984227f979c342b13e513a8288
SerapisGod <108385357+SerapisGod@users.noreply.github.com>
2024-09-27 20:25:30 -0600

    setting up lab

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This app is to be called the "Incredible Todo List App" By Matthew</Text>
       <StatusBar style="auto" />
     </View>
   );
